890fbfb2bfd3f88731ed1f5a98a7600351c4622f
Maha Zayed <maha@blackfynn.com>
2020-04-09 18:26:18 -0400

    Working on dashboard canvas buttons

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -2,11 +2,11 @@
   <div class="dashboard">
     <h2>Blackfynn Pictionary</h2>
     <div class="dashboard-components">
-      <dashboard-buttons v-if="toggle"/>
+      <dashboard-buttons v-if="toggle" />
       <dashboard-canvas />
     </div>
     <label class="switch">
-      <input type="checkbox" v-model="toggle">
+      <input type="checkbox" v-model="toggle" />
       <span class="slider round"></span>
     </label>
     <div class="input" v-if="!toggle">
@@ -17,97 +17,96 @@
 </template>
 
 <script>
-  import DashboardCanvas from '../Dashboard/DashboardCanvas.vue'
-  import DashboardButtons from '../Dashboard/DashboardButtons.vue'
-  export default {
-    name: 'Dashboard',
-    components: {
-      DashboardCanvas,
-      DashboardButtons
-    },
+import DashboardCanvas from "../Dashboard/DashboardCanvas.vue";
+import DashboardButtons from "../Dashboard/DashboardButtons.vue";
+export default {
+  name: "Dashboard",
+  components: {
+    DashboardCanvas,
+    DashboardButtons,
+  },
 
-    data() {
-      return {
-        toggle: false
-      }
-    },
+  data() {
+    return {
+      toggle: false,
+    };
+  },
 
-    computed: {
-      dashboardView: function() {
-        return this.toggle ? 'Player' : 'Viewer'
-      }
+  computed: {
+    dashboardView: function() {
+      return this.toggle ? "Player" : "Viewer";
     },
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .dashboard {
+  h2 {
+    margin-left: 2rem;
+  }
 
-h2 {
-  margin-left: 2rem;
-}
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
 
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
+  /* Hide default HTML checkbox */
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  input:checked + .slider {
+    background-color: #2196f3;
+  }
 
-input:checked + .slider {
-  background-color: #2196F3;
-}
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
 
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
 
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
+  .slider.round:before {
+    border-radius: 50%;
+  }
 }
 
 .dashboard-components {
@@ -122,7 +121,7 @@ input:checked + .slider:before {
 input {
   width: 14rem;
   height: 2rem;
-  border: solid 1px #0D4872;
+  border: solid 1px #0d4872;
   margin-top: 2rem;
 }
 </style>
--- src/components/Dashboard/DashboardButtons.vue
+++ src/components/Dashboard/DashboardButtons.vue
@@ -1,34 +1,48 @@
 <template>
   <div class="dashboard-buttons">
+    <div class="dashboard-buttons-header">
+      Canvas Tools
+    </div>
     <div class="column">
-      <button @click="selectUndo">
-        <svgicon name="undo-icon" width="20" height="30" color="#0D4872" />
-      </button>
-      <button @click="selectPencil">
-        <svgicon name="pencil-icon" width="20" height="30" color="#0D4872" />
-      </button>
-      <button @click="selectShape">
-        <svgicon name="shape-icon" width="20" height="30" color="#0D4872" />
-      </button>
-      <button @click="selectMove">
-        <svgicon name="move-icon" width="20" height="30" color="#0D4872" />
-      </button>
-
-      <button @click="selectClear">
-        <svgicon name="clear-icon" width="20" height="30" color="#0D4872" />
-      </button>
-      <button @click="selectPaintbrush">
-        <svgicon
-          name="paintbrush-icon"
-          width="20"
-          height="30"
-          color="#0D4872"
-        />
-      </button>
-      <button @click="selectErase">
-        <svgicon name="eraser-icon" width="20" height="30" color="#0D4872" />
-      </button>
-
+      <div class="row">
+        <button @click="selectUndo">
+          <svgicon name="undo-icon" width="20" height="30" color="#0D4872" />
+        </button>
+        <button @click="selectClear">
+          <svgicon name="clear-icon" width="20" height="30" color="#0D4872" />
+        </button>
+      </div>
+    </div>
+    <div class="column">
+      <div class="row">
+        <button @click="selectPencil">
+          <svgicon name="pencil-icon" width="20" height="30" color="#0D4872" />
+        </button>
+        <button @click="selectPaintbrush">
+          <svgicon
+            name="paintbrush-icon"
+            width="20"
+            height="30"
+            color="#0D4872"
+          />
+        </button>
+      </div>
+    </div>
+    <div class="column">
+      <div class="row">
+        <button @click="selectErase">
+          <svgicon name="eraser-icon" width="20" height="30" color="#0D4872" />
+        </button>
+        <button @click="selectShape">
+          <svgicon name="shape-icon" width="20" height="30" color="#0D4872" />
+        </button>
+      </div>
+    </div>
+    <div class="column">
+      <div class="row">
+        <button @click="selectMove">
+          <svgicon name="move-icon" width="20" height="30" color="#0D4872" />
+        </button>
       <button @click="selectColor">
         <svgicon
           name="color-picker-icon"
@@ -39,6 +53,16 @@
         />
       </button>
     </div>
+    </div>
+    <div class="dashboard-colors">
+      <div class="column">
+        <div class="row">
+           <button class="red-color" @click="selectColor(red)"/>
+           <button class="blue-color" @click="selectColor(blue)"/>
+        </div>
+      </div>
+     
+   </div>
   </div>
 </template>
 
@@ -48,12 +72,12 @@ export default {
   name: "DashboardButtons",
 
   computed: {
-    ...mapState(['canvas', 'canvasCtx']),
+    ...mapState(["canvas", "canvasCtx"]),
   },
 
   methods: {
     selectClear: function() {
-      this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
   },
 };
@@ -62,18 +86,50 @@ export default {
 <style lang="scss" scoped>
 .dashboard-buttons {
   margin-right: 1rem;
+  border: solid 1px black;
+  background-color: lightgray;
+ 
+}
+
+.dashboard-buttons-header {
+  border-bottom: solid 1px black;
+  background-color: darkgray;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  padding-top: 0.5rem;
+  margin-bottom: 1rem;
 }
 
 .column {
   display: flex;
   flex-direction: column;
+   padding-left: 0.5rem;
 }
 
 button {
   background-color: #79bff2;
   border: solid 1px #0d4872;
   border-radius: 2px;
-  margin-bottom: 5px;
+  margin-bottom: 1rem;
+  margin-right: 0.5rem;
   cursor: pointer;
+}
+
+.red-color {
+  background-color: red;
+  border: none;
+  cursor: pointer;
+  border-radius: 0;
+  width: 30px;
+  height: 30px;
+}
+
+.blue-color {
+  background-color: blue;
+  border: none;
+  cursor: pointer;
+  border-radius: 0;
+  width: 30px;
+  height: 30px;
 }
 </style>
